dc3aa1c7e4cbbe95b966137fe7c45e378d1207f6
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-02-18 12:05:01 +0000

    new logo :)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,21 @@ function Header() {
   return (
     <header>
       <div className="headerleft">
-        <i className="fas fa-3x fa-th" />
+        <span className="fa-layers fa-fw">
+          <i className="fas fa-square-full" data-fa-transform="" />
+          <i className="fas fa-square-full" data-fa-transform="left-18" />
+          <i className="fas fa-square-full" data-fa-transform="right-18" />
+          <i className="fas fa-square-full" data-fa-transform="up-18" />
+          <i className="fas fa-square-full" data-fa-transform="left-18 down-18" />
+          <i
+            className="fas fa-square-full"
+            data-fa-transform="right-18 down-18"
+            style={{ color: 'rgba(255, 255, 255, 0.05)' }}
+          />
+          <i className="fas fa-square-full" data-fa-transform="down-18" />
+          <i className="fas fa-square-full" data-fa-transform="left-18 up-18" />
+          <i className="fas fa-square-full" data-fa-transform="right-18 up-18" />
+        </span>
       </div>
       <div className="headercenter">
         <p className="title is-4 has-text-light">Quicksell Builder</p>
