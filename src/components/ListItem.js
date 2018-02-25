ed29c0640096b8388f01eca7c6b3543d4da72508
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-02-25 23:29:02 +0000

    added item code to listitems

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,9 +7,18 @@ class ListItem extends React.Component {
     return (
       <li className="listitem">
         <div className="listitemimage">
-          <img className="listitemimage" src={product.image || 'http://via.placeholder.com/84x64'} alt="" />
+          <img
+            className="listitemimage"
+            src={product.image || 'http://via.placeholder.com/84x64'}
+            alt=""
+          />
         </div>
-        <div className="listitemlabel">{product.label}</div>
+        <div className="listitemlabel">
+          {product.label}
+          <div className="listitemcode">
+            <sup>{product.item}</sup>
+          </div>
+        </div>
       </li>
     )
   }
