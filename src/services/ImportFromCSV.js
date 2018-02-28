1de15802dab7a603f6f26b583fc8ca397e5fe317
Lee Biddiscombe (Food IT) <BIDDL@live.co-op.local>
2018-02-28 13:43:11 +0000

    refactor fetch into separate functions

--- src/services/ImportFromCSV.js
+++ src/services/ImportFromCSV.js
@@ -8,30 +8,6 @@
 */
 import csvJSON from './csvJSON'
 
-function getGridPosition(top, left) {
-  const height = 107
-  const width = 100
-  const offsetLeft = 90
-  const offsetTop = 52
-  const cols = 7
-
-  let row = (top - offsetTop) / height
-  let col = (left - offsetLeft) / width
-  let pos = row * cols + col
-
-  return pos
-}
-
-function getUniqueProducts(products) {
-  return products.reduce(
-    (uniqueProducts, product) =>
-      uniqueProducts.findIndex(p => p.item === product.item) < 0
-        ? [...uniqueProducts, product]
-        : uniqueProducts,
-    []
-  )
-}
-
 function ImportFromCSV() {
   return new Promise((resolve, reject) => {
     const groups = [
@@ -49,80 +25,109 @@ function ImportFromCSV() {
       { id: '6', name: 'U-Z' }
     ]
 
-    var products = []
-    var layout = {}
-
     fetch('./data/QuickSellButtons.csv')
       .then(response => response.text())
       .then(response => {
         const results = csvJSON(response)
-
-        products = results
-          .filter(p => {
-            return (
-              groups.map(r => r.id).includes(p.MenuGroup) && pages.map(r => r.id).includes(p.Page)
-            )
-          })
-          .map(p => {
-            return {
-              item: p.ItemID,
-              label: p.Description,
-              image: p.Image !== 'NULL' ? '/images/' + p.Image : false,
-              group: p.MenuGroup,
-              page: p.Page,
-              seq: p.MenuOption,
-              top: p.Top,
-              left: p.Left
-            }
-          })
-
-        const uniqueProducts = getUniqueProducts(products)
-
-        const qsGroups = groups.map(group => {
-          const productPages = pages.map(p => {
-            var gridItems = []
-            const gridLength = 28 // TODO: set as grid size
-
-            for (let i = 0; i < gridLength; i++) {
-              gridItems.push({
-                page: p.id,
-                group: group.id,
-                seq: i + 1,
-                empty: true
-              })
-            }
-
-            products.filter(r => r.group === group.id && r.page === p.id).map(p => {
-              const pos = getGridPosition(p.top, p.left)
-              p.seq = pos + 1
-              gridItems[pos] = p
-              return true
-            })
-
-            return {
-              id: p.id,
-              name: p.name,
-              products: gridItems
-            }
-          })
-
-          return {
-            id: group.id,
-            name: group.name,
-            pages: productPages
-          }
-        })
-
-        layout.groups = qsGroups
+        const filteredResults = getFilteredResults(groups, pages, results)
+        const products = getUniqueProducts(filteredResults)
+        const layout = { groups: getLayoutGroups(groups, pages, filteredResults) }
 
         resolve({
           groups,
           pages,
-          products: uniqueProducts,
+          products,
           layout
         })
       })
   })
+}
+
+function getUniqueProducts(products) {
+  return products.reduce(
+    (uniqueProducts, product) =>
+    uniqueProducts.findIndex(p => p.item === product.item) < 0 ? [...uniqueProducts, product] :
+    uniqueProducts, []
+  )
+}
+
+function getFilteredResults(groups, pages, results) {
+  return results
+    .filter(p => {
+      return (
+        groups.map(r => r.id).includes(p.MenuGroup) && pages.map(r => r.id).includes(p.Page)
+      )
+    })
+    .map(p => {
+      return {
+        item: p.ItemID,
+        label: p.Description,
+        image: p.Image !== 'NULL' ? '/images/' + p.Image : false,
+        group: p.MenuGroup,
+        page: p.Page,
+        seq: p.MenuOption,
+        top: p.Top,
+        left: p.Left
+      }
+    })
+}
+
+function getLayoutGroups(groups, pages, filteredResults) {
+  return groups.map(group => {
+    return {
+      id: group.id,
+      name: group.name,
+      pages: getLayoutPages(group, pages, filteredResults)
+    }
+  })
+}
+
+function getLayoutPages(group, pages, filteredResults) {
+  return pages.map(page => {
+    return {
+      id: page.id,
+      name: page.name,
+      products: getLayoutProducts(group, page, filteredResults)
+    }
+  })
+
+}
+
+function getLayoutProducts(group, page, filteredResults) {
+  var layoutProducts = []
+  const gridLength = 28 // TODO: set as grid size
+
+  for (let i = 0; i < gridLength; i++) {
+    layoutProducts.push({
+      page: page.id,
+      group: group.id,
+      seq: i + 1,
+      empty: true
+    })
+  }
+
+  filteredResults.filter(r => r.group === group.id && r.page === page.id).forEach(p => {
+    const pos = getGridPosition(p.top, p.left)
+    p.seq = pos + 1
+    layoutProducts[pos] = p
+  })
+
+  return layoutProducts
+
+}
+
+function getGridPosition(top, left) {
+  const height = 107
+  const width = 100
+  const offsetLeft = 90
+  const offsetTop = 52
+  const cols = 7
+
+  let row = (top - offsetTop) / height
+  let col = (left - offsetLeft) / width
+  let pos = row * cols + col
+
+  return pos
 }
 
 export default ImportFromCSV
