0546b5cd7ce7ebf178d40130f9983867d89ba53a
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-03-08 12:57:54 +0000

    Show group headings in list items

--- src/components/List/ListItems.js
+++ src/components/List/ListItems.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import ListItem from './ListItem'
+import settings from '../../settings'
 
 class ListItems extends React.Component {
   render() {
     const products = this.props.products
+    let lastGroup = null
+    const rows = []
+
+    products
+      .filter(product => {
+        return (
+          product.label.toLowerCase().indexOf(this.props.filter.toLowerCase()) !== -1 ||
+          product.item.indexOf(this.props.filter) !== -1
+        )
+      })
+      .sort((a, b) => a.group - b.group || a.label.localeCompare(b.label))
+      .forEach((product, i) => {
+        if (product.group !== lastGroup) {
+          rows.push(
+            <li className="listcategory" key={'cat' + i}>
+              {settings.importGroups.find(g => g.id === product.group).name}
+            </li>
+          )
+          lastGroup = product.group
+        }
+        rows.push(<ListItem key={i} product={product} />)
+      })
 
     return (
       <div className="panel-block">
-        <ul className="listitems">
-          {products
-            .filter(product => {
-              return (
-                product.label.toLowerCase().indexOf(this.props.filter.toLowerCase()) !== -1 ||
-                product.item.indexOf(this.props.filter) !== -1
-              )
-            })
-            .map((product, i) => {
-              return <ListItem key={i} product={product} />
-            })}
-        </ul>
+        <ul className="listitems">{rows}</ul>
       </div>
     )
   }
