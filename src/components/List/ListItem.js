7d4d0a1bf7ebfb06926342942f29b721da89018a
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-03-09 12:01:23 +0000

    added edit mode toggle (no edit logic) to listitems

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 class ListItem extends React.Component {
+  constructor() {
+    super()
+    this.toggleEdit = this.toggleEdit.bind(this)
+    this.state = {
+      inEdit: false
+    }
+  }
+
+  toggleEdit(e) {
+    e.preventDefault()
+    this.setState({
+      inEdit: !this.state.inEdit
+    })
+  }
+
   render() {
     const product = this.props.product
+    const inEdit = this.state.inEdit
+    const editIcon = <i className="far fa-edit" />
+    const saveIcon = <i className="far fa-save" />
 
     return (
       <li className="listitem">
@@ -10,10 +28,16 @@ class ListItem extends React.Component {
           <img src={product.image || 'https://via.placeholder.com/84x64'} alt="" />
         </div>
         <div className="listitemlabel">
-          {product.label}
+          {product.label + (inEdit ? ' in edit' : '')}
           <div className="listitemcode">
             <sup>{product.item}</sup>
           </div>
+        </div>
+        <div className="listitemaction">
+          <a className="dark" onClick={this.toggleEdit}>
+            <span className={inEdit ? 'hidden' : ''}>{editIcon}</span>
+            <span className={inEdit ? '' : 'hidden'}>{saveIcon}</span>
+          </a>
         </div>
       </li>
     )
