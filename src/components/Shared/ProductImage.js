080977e37c2010a605e7ac117de247345cd49af5
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-03-11 22:25:12 +0000

    rolled back changes as didn't work

--- src/components/Shared/ProductImage.js
+++ src/components/Shared/ProductImage.js
@@ -1,49 +1,15 @@
 import React from 'react'
 
 /*
-
 TODO: check image exists and if not replace with placeholder
-
 */
 
-function getImage(url) {
-  return new Promise(function(resolve) {
-    if (!url) {
-      resolve(null)
-    }
-
-    var img = new Image()
-    img.onload = function() {
-      resolve(url)
-    }
-    img.onerror = function() {
-      resolve('https://via.placeholder.com/84x64')
-    }
-    img.src = url
-  })
-}
-
 class ProductImage extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      imageSrc: ''
-    }
-  }
-
-  componentDidMount() {
-    const product = this.props.product
-    getImage(product.image).then(result => {
-      this.setState({
-        imageSrc: result
-      })
-    })
-  }
-
   render() {
+    const product = this.props.product
     return (
       <div className="itemimg">
-        <img src={this.state.imageSrc} alt="" />
+        <img src={product.image ? product.image : undefined} alt="" />
       </div>
     )
   }
