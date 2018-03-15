aeb0aa65a08606a7d7fc03b220b165fef9b591f0
LBiddiscombe <BIDDL@live.co-op.local>
2018-03-15 18:53:06 +0000

    fix filter styling

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 class Filter extends React.Component {
-
   componentDidUpdate() {
     const main = document.getElementById('main')
     const filterwrap = document.getElementById('filterwrap')
@@ -11,19 +10,12 @@ class Filter extends React.Component {
   render() {
     return (
       <div id="filterwrap">
-        <div className="panel-block filter">
-          <p className="control has-icons-left">
-            <input
-              className="input"
-              type="text"
-              placeholder="search name or code"
-              onChange={this.props.handleFilterChange}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-search" />
-            </span>
-          </p>
-        </div>
+        <input
+          className="input"
+          type="text"
+          placeholder="search name or code"
+          onChange={this.props.handleFilterChange}
+        />
       </div>
     )
   }
