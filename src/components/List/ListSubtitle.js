8910f1b7ccaade9ad987be1c48f59a94350a02c8
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-03-27 12:14:48 +0100

    added styling

--- src/components/List/ListSubtitle.js
+++ src/components/List/ListSubtitle.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import settings from '../../settings'
 
+const Li = styled.li`
+   {
+    font-size: 1.2rem;
+    font-family: 'AvenirNextLTW01-Medium';
+    border-bottom: 2px solid var(--accent-color);
+    background-color: var(--aside-bg);
+    position: sticky;
+    top: 0px;
+    z-index: 2;
+    &:not(:first-child) {
+      margin-top: 1rem;
+    }
+  }
+`
 function ListSubtitle(props) {
-  return (
-    <li className="listcategory">{settings.importGroups.find(g => g.id === props.group).name}</li>
-  )
+  return <Li>{settings.importGroups.find(g => g.id === props.group).name}</Li>
 }
 
 export default ListSubtitle
